db4a16602719023380abcfc1678d8af2a4f31cce
Kai Chen <foreverbonfy@gmail.com>
2021-01-14 14:43:16 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+# coding: utf-8
+
+import glob
+
+def get_files(pattern):
+    return glob.glob(pattern)
+    
+if __name__ == '__main__':
+    pattern = '**/*.template'
+    
+    
+    if len(sys.argv) == 1:
+        print('No input, will use default pattern:', pattern)
+    else:
+        pattern = sys.argv[1]
+        print('Use pattern:', pattern)
+
+    filelist = get_files(pattern)
+    if not filelist:
+        print('Please check pattern, no files found')
+        raise Exeption('No Files Found')
+        
+    for filename in filelist:
+        print('scan ', filename)
